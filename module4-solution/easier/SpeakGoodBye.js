c880c3d394ee11f92073a851f24871f738fdb4ce
mustafaabdulaziz <mustafaabdulaziz896745@gmail.com>
2018-12-02 18:55:25 +0300

    module4-solution

---
+++ module4-solution/easier/SpeakGoodBye.js
@@ -0,0 +1,13 @@
+var byeSpeaker = {};
+byeSpeaker.speakWord = "Good Bye";
+
+// STEP 8: Rewrite the 'speak' function such that it is attached to the
+// byeSpeaker object instead of being a standalone function.
+// See Lecture 52, part 2
+byeSpeaker.speak = function(name) {
+  console.log(byeSpeaker.speakWord + " " + name);
+}
+
+// STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
+// 'byeSpeaker' on the global scope as well.
+// xxxx.xxxx = byeSpeaker;
---
+++ module4-solution/easier/SpeakHello.js
@@ -0,0 +1,8 @@
+var helloSpeaker = {};
+helloSpeaker.speakWord = "Hello";
+
+
+
+helloSpeaker.speak = function (name) {
+  console.log(helloSpeaker.speakWord + " " + name);
+}
